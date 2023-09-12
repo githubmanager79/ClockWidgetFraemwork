13c5ac22b4afb6ac4d76ebb104d2b1f5bd6ad7c1
bpotorochyn <p.bogdanvladimirovich@gmail.com>
2023-09-12 18:26:17 +0300

    1) Fix some problems

--- Sources/ClockWidgetFraemwork.framework/Headers/ClockWidgetFraemwork.h
+++ Sources/ClockWidgetFraemwork.framework/Headers/ClockWidgetFraemwork.h
@@ -13,6 +13,7 @@ FOUNDATION_EXPORT double ClockWidgetFraemworkVersionNumber;
 //! Project version string for ClockWidgetFraemwork.
 FOUNDATION_EXPORT const unsigned char ClockWidgetFraemworkVersionString[];
 
+
 // In this header, you should import all the public headers of your framework using statements like #import <ClockWidgetFraemwork/PublicHeader.h>
 
 
